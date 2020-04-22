9932d3796424ace48c8e35300bfc6f17975e14ef
Tej <tejpartapsingh91@gmail.com>
2020-04-22 11:47:15 -0400

    revise

--- portfolio/src/components/About/About.js
+++ portfolio/src/components/About/About.js
@@ -1,22 +1,55 @@
-import React from 'react';
-import './About.css';
+import React from "react";
+import "./About.css";
 
-const About = () =>
+const About = () => (
   <>
     <section className="about">
-      <section className='sticker'>
+      <section className="sticker">
         <h1 className="sticker__text h2">Welcome</h1>
       </section>
       <section className="about__wrap">
         <section className="about__content">
-          <p>Hello...</p>
+          <p>Hello, Lovely Human!</p>
           <p>Thanks for taking the time to check my portfolio page.</p>
-          <p>I am a <a href="https://roadmap.sh/frontend" target="_blank" rel="noopener noreferrer">Front End Developer</a> based in Toronto.</p>
-          <p>I love experimenting with emerging technologies built using JavaScript. I am always keen in building JS based services to solve a real world problem or automate niche market requirements. My objective is to become a better <strong>Front + Back End Developer</strong> specializing in <strong>MERN</strong> stack.</p>
-          <p>I am a huge advocate of continuous learning, making web an awesome place for humans.</p>
+          <p>
+            I am a&nbsp;
+            <a
+              href="https://roadmap.sh/frontend"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Front End Developer
+            </a>
+            &nbsp;based in Toronto.
+          </p>
+          <p>
+            I like JavaScript. I am actively learning the language and
+            discovering new nuances every now and then, through small projects
+            based learning. I am always keen in building JavaScript based
+            applications to solve a real world problem or fill the market gap
+            through the presence of a web service. My objective is to become a
+            better <strong>Front End Developer</strong>,&nbsp;
+            <strong>Back End Developer</strong>,&nbsp;
+            <strong>Full Stack Developer</strong> specializing in&nbsp;
+            <strong>MERN</strong> stack.
+          </p>
+          <p>
+            I am a huge advocate of continuous learning, making web an awesome
+            place for humans.
+          </p>
+          <p>
+            As of today, I have uncovered hidden gems of&nbsp;
+            <strong>
+              REST API, UNIX, Express, MongoDB, React, CSS Grid, JavaScript,
+              Gatsby, Heroku, AWS, ES6
+            </strong>
+            . My goal for this year is to gain expertise in Redux, Modern React
+            Practices, TDD, Advanced JavaScript, TypeScript, GraphQL.
+          </p>
         </section>
       </section>
     </section>
   </>
+);
 
 export default About;
--- portfolio/src/components/Work/Work.js
+++ portfolio/src/components/Work/Work.js
@@ -9,6 +9,37 @@ const Work = () => (
         <h1 className="sticker__text h2">Projects</h1>
       </section>
       <section className="work__container">
+        <div className="card card--0">
+          <header>
+            <h2 className="h3">Express</h2>
+            <main class="card__img--bg">
+              <img
+                className="card__img"
+                src="https://api.iconify.design/logos-express.svg"
+                alt="Express Projects"
+              />
+            </main>
+            <p>Archive of my Express projects.</p>
+            <ul className="badges">
+              <li>Express</li>
+              <li>Node</li>
+            </ul>
+          </header>
+          <footer>
+            <div className="buttons">
+              <span className="button__icon">
+                <MdLink />
+              </span>
+              <a
+                href="https://github.com/tpkahlon/express"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                View Source
+              </a>
+            </div>
+          </footer>
+        </div>
         <div className="card card--1">
           <header>
             <h2 className="h3">JavaScript</h2>
@@ -128,12 +159,12 @@ const Work = () => (
                 rel="noopener noreferrer"
               >
                 View Source
-                  </a>
+              </a>
             </div>
           </footer>
         </div>
+      </section>
     </section>
-  </section>
   </>
 );
 

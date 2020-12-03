9344320f5f2c3bac6f3fb95bc9fb5f0ab26ae5a9
tkahlon <tkahlon@labx.com>
2020-12-02 19:49:03 -0500

    revise links

--- portfolio/src/components/Contact/Contact.js
+++ portfolio/src/components/Contact/Contact.js
@@ -1,14 +1,7 @@
 import React from "react";
 import "./Contact.css";
 import {
-  FaLinkedin,
-  FaWhatsapp,
   FaGithub,
-  FaBehanceSquare,
-  FaFileCode,
-  FaBlogger,
-  FaDev,
-  FaCopyright,
 } from "react-icons/fa";
 
 const Contact = () => (
@@ -32,81 +25,11 @@ const Contact = () => (
             <li className="contact__list-item">
               <a
                 className="text-decoration-none"
-                href="https://wa.me/14164092721"
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                <FaWhatsapp />
-              </a>
-            </li>
-            <li className="contact__list-item">
-              <a
-                className="text-decoration-none"
                 href="https://github.com/tpkahlon"
                 rel="noopener noreferrer"
                 target="_blank"
               >
                 <FaGithub />
-              </a>
-            </li>
-            <li className="contact__list-item">
-              <a
-                className="text-decoration-none"
-                href="https://linkedin.com/in/tpkahlon"
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                <FaLinkedin />
-              </a>
-            </li>
-            <li className="contact__list-item">
-              <a
-                className="text-decoration-none"
-                href="https://behance.net/tpkahlon"
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                <FaBehanceSquare />
-              </a>
-            </li>
-            <li className="contact__list-item">
-              <a
-                className="text-decoration-none"
-                href="https://www.dropbox.com/s/zfc62n6wzujs9qn/Resume.pdf?dl=0"
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                <FaFileCode />
-              </a>
-            </li>
-            <li className="contact__list-item">
-              <a
-                className="text-decoration-none"
-                href="https://dev.to/tpkahlon"
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                <FaDev />
-              </a>
-            </li>
-            <li className="contact__list-item">
-              <a
-                className="text-decoration-none"
-                href="https://tejkahlon.netlify.com/"
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                <FaCopyright />
-              </a>
-            </li>
-            <li className="contact__list-item">
-              <a
-                className="text-decoration-none"
-                href="https://satrangi.netlify.com/"
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                <FaBlogger />
               </a>
             </li>
           </ul>
